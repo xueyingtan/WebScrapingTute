cb096d0822f2faebde1d6941bf9522087c85436f
Jane Tan <xueyingt96@gmail.com>
2021-02-05 17:10:52 +1000

    Added validation for null sections and feedback to console while running

--- scraper.py
+++ scraper.py
@@ -10,7 +10,7 @@ import pandas as pd
 @click.command()
 @click.option(
     '-l','--limit', nargs=1, default=float('inf'),
-    help='A maximum number of result pages to collect. Defaults to no limit / collect all pages.'
+    help='A maximum number of results to collect. Minimum number of the results is 10. Defaults to no limit / collect all results.'
 )
 @click.option(
     '-f','--filename', nargs=1,
@@ -25,23 +25,25 @@ def main(limit, filename, query):
 
     table = [] # temporary data structure to store the data
     while limit > 0 and urlquery:
+        print("Request page: "+urlquery)
         response = requests.get(urlquery) # send HTTP request to the web server and get response
         soup = BeautifulSoup(response.text, 'html.parser') # Get the html content and wrap in soup object
         articles = soup.select("div#content-results article")
         for article in articles:
             row = []
-            row.append(article.time['datetime']) #datetime
-            row.append(article.a['title']) #title
-            row.append(article.select_one("p.byline").a.text) #author
-            row.append(article.select_one("p.byline").em.text) #organisation
-            row.append(article.header.next_sibling.next_sibling.text) #text (next sibling of header is nextline character)
-            row.append(url+"/"+article.a["href"].split("%2F")[-1]) #article's link
+            row.append(article.time['datetime']) if article.time['datetime'] else "" #datetime
+            row.append(article.a['title']) if article.a['title'] else "" #title
+            row.append(article.select_one("p.byline").a.text) if article.select_one("p.byline").a.text else ""#author
+            row.append(article.select_one("p.byline").em.text) if article.select_one("p.byline").em else "" #organisation
+            row.append(article.header.next_sibling.next_sibling.text) if article.header.next_sibling.next_sibling.text else "" #text (next sibling of header is nextline character)
+            row.append(url+"/"+article.a["href"].split("%2F")[-1]) if article.a["href"] else "" #article's link
             table.append(row)
         limit -= len(articles) # keep track of the number of articles so far if there is a limit
         urlquery = url+soup.find('a',attrs={'rel':'next'})['href'] # get next page if there is one (Each page returns 10 article posts)
         
     # Convert data to dataframe and write to csv file
     df = pd.DataFrame(np.array(table), columns=["Datetime","Title","Author","Author Organisation","Description Text","Article Link"])
+    print("Writing to CSV file...")
     df.to_csv(filename, index=None)
 
 
