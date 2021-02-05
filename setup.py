4fdc02e512960c99cf1d329f78a831875c8f776e
betsy <elizabeth.alpert@qut.edu.au>
2021-02-05 15:32:14 +1000

    Add additional metadata to setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,14 @@ from setuptools import setup
 
 setup(
     name = 'scraper',
+    author='Jane Tan',
+    url='https://github.com/xueyingtan/WebScrapingTute',
     version = '0.1.0',
     entry_points = {
         'console_scripts': [
             'scrape = scraper:main'
         ]
     },
-    python_requires=">=3.8"
+    python_requires=">=3.8",
+    license="MIT"
 )
